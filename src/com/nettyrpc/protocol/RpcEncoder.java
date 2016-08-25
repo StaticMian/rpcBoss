ffa0c0b28de671187a745f8b428911eccc391c43
LiHongLinLsm <591744830@qq.com>
2016-08-25 19:47:06 +0800

    first commit

---
+++ src/com/nettyrpc/protocol/RpcEncoder.java
@@ -0,0 +1,30 @@
+package com.nettyrpc.protocol;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+/**
+ * RPC 编码器
+ *
+ * @author huangyong
+ * @since 1.0.0
+ */
+public class RpcEncoder extends MessageToByteEncoder {
+
+    private Class<?> genericClass;
+
+    public RpcEncoder(Class<?> genericClass) {
+        this.genericClass = genericClass;
+    }
+
+    @Override
+    public void encode(ChannelHandlerContext ctx, Object in, ByteBuf out) throws Exception {
+        if (genericClass.isInstance(in)) {
+            byte[] data = SerializationUtil.serialize(in);
+            //byte[] data = JsonUtil.serialize(in); // Not use this, have some bugs
+            out.writeInt(data.length);
+            out.writeBytes(data);
+        }
+    }
+}
---
+++ src/com/nettyrpc/server/RpcService.java
@@ -0,0 +1,20 @@
+package com.nettyrpc.server;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.stereotype.Component;
+
+/**
+ * RPC 请求注解（标注在服务实现类上）
+ *
+ * @author huangyong
+ * @since 1.0.0
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface RpcService {
+    String value();
+}
